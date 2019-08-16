fad2f9ea2145071a17fa8f4119cd11d04c7c954c
Guy Bianco IV <gjbiancoiv@gmail.com>
2019-08-16 00:45:01 -0400

    send a POST body

--- src/hubot-simple-namer.js
+++ src/hubot-simple-namer.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = (robot) => {
   robot.hear(/^!name$/i, (res) => {
     const query = escape(res.match[1]);
-    axios.post('https://api.codetunnel.net/random-nick')
+    axios.post('https://api.codetunnel.net/random-nick', {})
       .then(result => res.send(result.data.nickname))
       .catch(err => {
         console.log('error retrieving namer: ' + err);
