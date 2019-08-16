215ae19283bd57f3fde2ddd4a72d179e05991182
Guy Bianco IV <gjbiancoiv@gmail.com>
2019-08-16 00:35:22 -0400

    initial

---
+++ src/hubot-simple-namer.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+
+module.exports = (robot) => {
+  robot.hear(/^!name$/i, (res) => {
+    const query = escape(res.match[1]);
+    axios.post('https://api.codetunnel.net/random-nick')
+      .then((result) => res.send(result.data.nickname));
+  })
+}
